b195edce2a6b3f61cbcc1c62cca2be39e2f4a827
Javier Mendiara <jmendiara@gmail.com>
2016-07-14 17:18:33 +0200

    [[TEST]] Move getLevel test to test API spec (#28)

--- test/logops.spec.js
+++ test/logops.spec.js
@@ -233,3 +233,11 @@ describe('Select log level', function() {
     });
   });
 });
+
+describe('Get log format with the API', function() {
+  it('should return the current log level', function() {
+    logger.setLevel('DEBUG');
+
+    expect(logger.getLevel()).to.equal('DEBUG');
+  });
+});
